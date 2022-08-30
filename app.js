ccc6c91cdf9ef9436f1ea0df6543056c2f134761
Grey <58967769+FlameRender@users.noreply.github.com>
2022-08-30 16:58:15 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const app = express()
+const path = require('node:path')
+
+console.log('hello from node');
 
 app.get('/', function (req, res) {
-  res.send('Hello World from Node/Express/Heroku')
+
+
+  res.sendFile(path.join(__dirname, 'index.html'));
+
 })
 
-app.listen(process.env.PORT || 3000,
-  () => console.log("Server is running..."));
+app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running..."));
