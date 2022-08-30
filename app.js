965717b32c03894e46b6e1b29532c468ae0ee838
Holmes <gholmes1@una.edu>
2022-08-30 13:39:32 -0500

    changes made for heroku

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const app = express()
+const path = require('node:path')
+
+console.log('hello from node');
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+
+
+  res.sendFile(path.join(__dirname, 'index.html'));
+
 })
 
-app.listen(3000)
+app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running..."));
